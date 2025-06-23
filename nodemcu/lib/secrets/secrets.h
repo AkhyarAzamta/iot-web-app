9d96fa5b47c9701cc4ac0a6a77690c495759c743
akhyarazamta <akhyar.azamta@gmail.com>
2025-06-23 19:55:57 +0700

    .

---
+++ nodemcu/lib/secrets/secrets.h
@@ -0,0 +1,4 @@
+#define MQTT_HOST     "b5535d64c66a4c7d87401602cdff2d9f.s1.eu.hivemq.cloud"
+#define MQTT_PORT     8883
+#define MQTT_USERNAME "hivemq.webclient.1750339350673"
+#define MQTT_PASSWORD "o2AwRMd%u73z.*5?nOYS"
